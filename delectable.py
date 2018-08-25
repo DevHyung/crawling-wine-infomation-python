2cd6b354cfe6ffc841b94cb0493ffc3997a3a005
DevHyung <khuphj@gmail.com>
2018-08-26 01:11:14 +0900

    링크용끝

--- delectable.py
+++ delectable.py
@@ -45,60 +45,70 @@ def save_excel(_FILENAME,_DATA,_HEADER):
 
 def get_category():
     linkList = []
+    titleList = []
     bs4 = get_bs_obejct_by_url('https://delectable.com/categories/')
     divs = bs4.find_all('div', class_='categories-list__section__category')
     for div in divs:
         linkList.append('https://delectable.com' + div.a['href'])
-    return linkList
+        titleList.append(div.a.get_text().strip())
+    return linkList,titleList
 
 if __name__ == "__main__":
-    ''' --------------------------------- INPUT YOUR CONFIG --------------------------------- '''
-    FILENAME = "delectable.xlsx"
-    HEADER = ['카테고리','와인 이름', '와이너리 이름', '생산지 정보', '품종 정보', '푸드 페어링', '평가 정보']
-    SCROLL_PAUSE_TIME = 0.1
-    #save_excel(FILENAME,None,HEADER)
-    ''' ------------------------------------------------------------------------------------- '''
+    #linkList, titleList = get_category()
+    #f = open("category.txt",'w',encoding='utf8')
+    #for idx in range(len(linkList)):
+        #f.write("{}@@@{}\n".format(titleList[idx],linkList[idx]))
+    #f.close()
+    #exit(-1)
+    f = open('category.txt')
+    lines = f.readlines()
 
+    SCROLL_PAUSE_TIME = 0.1
     driver = webdriver.Chrome('./chromedriver')
     driver.maximize_window()
-    driver.get('https://delectable.com/categories/rich-and-bold')
-    time.sleep(3)
-
-    # Get scroll height
-    idx = 1
-    while True:
-        # Scroll down to bottom
-        last_height = driver.execute_script("return document.body.scrollHeight")
-        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-
+    for line in lines:
+        title, url = line.split('@@@')
+        print(" {} 카테고리 파싱중 ".format(title))
+        driver.get(url.strip())
+        time.sleep(3)
+        # Get scroll height
+        idx = 1
         while True:
-            # Wait to load page
-            time.sleep(SCROLL_PAUSE_TIME)
+            # Scroll down to bottom
+            last_height = driver.execute_script("return document.body.scrollHeight")
+            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
-            # Calculate new scroll height and compare with last scroll height
-            new_height = driver.execute_script("return document.body.scrollHeight")
-            if new_height == last_height:
-                idx += 1
+            while True:
+                # Wait to load page
+                time.sleep(SCROLL_PAUSE_TIME)
+
+                # Calculate new scroll height and compare with last scroll height
+                new_height = driver.execute_script("return document.body.scrollHeight")
+                if new_height == last_height:
+                    idx += 1
+                    break
+                else:
+                    idx = 1
+                last_height = new_height
+            if idx == 50:
                 break
-            else:
-                idx = 1
-            last_height = new_height
-        if idx == 50:
-            break
-    time.sleep(3)
-    # === save
-    linkList = []
-    bs4 = BeautifulSoup(open('html.txt', encoding='utf8').read(), 'lxml')
-    divs = bs4.find_all('div', class_='capture capture--feed-capture')
-    for div in divs:
-        # print(div.find('h1',class_='capture-header__name').get_text())
-        if div.a['href'] != '':
-            print('https://delectable.com' + div.a['href'])
-            linkList.append('https://delectable.com' + div.a['href'])
+        time.sleep(3)
+        # === save
+        linkList = []
+        bs4 = BeautifulSoup(driver.page_source, 'lxml')
+        divs = bs4.find_all('div', class_='capture capture--feed-capture')
+        for div in divs:
+            # print(div.find('h1',class_='capture-header__name').get_text())
+            if div.a['href'] != '':
+                #print('https://delectable.com' + div.a['href'])
+                linkList.append('https://delectable.com' + div.a['href'])
+        print("총 {}개 수집완료".format(len(linkList)))
+        fileTitle = bs4.find('h1',class_='feed__header__titlenopadding').get_text().strip()
+        print("파일명은 ",fileTitle,'입니다')
+        linkFile = fileTitle
+        f = open(linkFile+'.txt','w')
+        for link in linkList:
+            f.write(link+'\n')
+        f.close()
 
-    linkFile = input(">>> 파일명을 입력하세요 ::")
-    f = open(linkFile,'w')
-    for link in linkList:
-        f.write(link+'\n')
-    f.close()
     driver.quit()
