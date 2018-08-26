2d0918c516959151f3b5cbccc8c893f316ea70d8
DevHyung <khuphj@gmail.com>
2018-08-26 21:02:15 +0900

    배포판으로

--- vivino.py
+++ vivino.py
@@ -2,7 +2,6 @@ import requests
 from bs4 import BeautifulSoup
 from openpyxl import load_workbook,Workbook
 import os
-import shutil
 def save_excel(_FILENAME, _DATA, _HEADER):
     if os.path.exists(_FILENAME):
         if _DATA == None:
@@ -35,91 +34,77 @@ header = {
 'content-type': 'application/json'
 }
 '''----------------- 환경설정 부분 ---------------------'''
-FILENAME = "vivino1000~.xlsx"
+FILENAME = input(">>> 저장할 파일이름만 적어주세요(확장자미포함) :")+'.xlsx'
 HEADER = ['Winery','Name', 'Region', 'Country', 'Rating', 'Summary']
 # init
 save_excel(FILENAME,None,HEADER)
-f2 = open('error.txt','a')
+f2 = open(FILENAME+'_except.txt','a')
 '''---------------------------------------------------'''
-START = 1000
-END = 1100
-while True:
-    print(">>> {} 파일 실행중".format("./미완/{}~{}.txt".format(START,END)))
-    lines = open("./미완/{}~{}.txt".format(START,END)).readlines()
-    tmp = []
-    idx = 1
-    for url in lines:
-        print("{}번째 : {} 실행중..".format(idx, url.strip()))
-        idx += 1
-        dataList = []
-        #
-        html = requests.get(url.strip(),headers=header)
-        bs4 = BeautifulSoup(html.text,'lxml')
-        #
-        try:
-            name = bs4.find('span',class_='wine-page__header__information__details__name__vintage').get_text().strip()
-        except:#새로운페이지
-            name = '-'
-            print(">>> Move")
-            f2.write(url)
-            continue
-        try:
-            winery = bs4.find('a',class_='wine-page__header__information__details__name__winery').get_text().strip()
-        except:
-            winery = '-'
-            print("C1",end='@')
+# 1 ~ 500  = 0번부터 499 까지
+# 500 ~ 1000 = 499부터
+#
+print(">>> EX )  시작 1, 끝 5을  1번째부터 5번째까지 파싱합니다.")
+START = int(input(">>> 시작 URL 번호(행) 입력 :")) - 1
+END = int(input(">>> 끝 URL 번호(행) 입력 : "))
+lines = open('vivino_url_list.txt').readlines()[START:END]
+idx = START + 1
+for url in lines:
+    print("{}번째 : {} 실행중..".format(idx, url.strip()))
+    idx += 1
+    dataList = []
+    #
+    html = requests.get(url.strip(),headers=header)
+    bs4 = BeautifulSoup(html.text,'lxml')
+    #
+    try:
+        name = bs4.find('span',class_='wine-page__header__information__details__name__vintage').get_text().strip()
+    except:#새로운페이지
+        name = '-'
+        print("\t>>> Move Except File")
+        f2.write(url)
+        continue
+    try:
+        winery = bs4.find('a',class_='wine-page__header__information__details__name__winery').get_text().strip()
+    except:
+        winery = '-'
+    try:
+        region = bs4.find('a',class_='wine-page__header__information__details__location__region').get_text().strip()
+    except:
+        region = '-'
+    try:
+        country = bs4.find("a",class_='wine-page__header__information__details__location__country').get_text().strip()
+    except:
+        country = '-'
+    try:
+        averLabel = bs4.find('div',class_='wine-page__header__information__details__average-rating__label').get_text().strip()
+    except:
+        averLabel = '-'
+    try:
+        averValue = bs4.find('div',class_='wine-page__header__information__details__average-rating__value__number').get_text().strip()
+    except:
+        averValue = '-'
+    #
+    divs = bs4.find_all('div',class_='wine-page__summary__item')
+    summary = ""
+    for div in divs:
+        summaryHeader = div.find('div',class_='wine-page__summary__item__header').get_text().strip()
+        summary += summaryHeader+":"
+        isVoid = True
+        for a in div.find_all('a'):
+            summary +=a.get_text().strip()+','
+            isVoid = False
+        for a in div.find_all('span'):
+            summary += a.get_text().strip() + ','
+            isVoid = False
+        if isVoid:
+            summary += div.find('div',class_='wine-page__summary__item__content').get_text().strip() + ','
+        summary = summary[:-1]+'\n'
 
-        try:
-            region = bs4.find('a',class_='wine-page__header__information__details__location__region').get_text().strip()
-        except:
-            region = '-'
-            print("C2", end='@')
-        try:
-            country = bs4.find("a",class_='wine-page__header__information__details__location__country').get_text().strip()
-        except:
-            country = '-'
-            print("C3", end='@')
-        try:
-            averLabel = bs4.find('div',class_='wine-page__header__information__details__average-rating__label').get_text().strip()
-        except:
-            averLabel = '-'
-            print("C4", end='@')
-        try:
-            averValue = bs4.find('div',class_='wine-page__header__information__details__average-rating__value__number').get_text().strip()
-        except:
-            averValue = '-'
-            print("C5", end='@')
-
-        #
-        divs = bs4.find_all('div',class_='wine-page__summary__item')
-        summary = ""
-        for div in divs:
-            summaryHeader = div.find('div',class_='wine-page__summary__item__header').get_text().strip()
-            summary += summaryHeader+":"
-            isVoid = True
-            for a in div.find_all('a'):
-                summary +=a.get_text().strip()+','
-                isVoid = False
-            for a in div.find_all('span'):
-                summary += a.get_text().strip() + ','
-                isVoid = False
-            if isVoid:
-                summary += div.find('div',class_='wine-page__summary__item__content').get_text().strip() + ','
-            summary = summary[:-1]+'\n'
-
-        dataList.append(winery)
-        dataList.append(name)
-        dataList.append(region)
-        dataList.append(country)
-        dataList.append(averLabel+' '+averValue)
-        dataList.append(summary)
-        #tmp.append(dataList)
-        save_excel(FILENAME,dataList,None)
-    START = END
-    END += 100
-    if END > 737780 and END == 737880 + 1:
-        break
-    elif END > 737780:
-        END = 737780 + 1
-
+    dataList.append(winery)
+    dataList.append(name)
+    dataList.append(region)
+    dataList.append(country)
+    dataList.append(averLabel+' '+averValue)
+    dataList.append(summary)
+    save_excel(FILENAME,dataList,None)
 f2.close()
